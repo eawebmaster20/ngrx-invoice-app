90fc1df37d281d8bfd398fe4da1c216612d8d934
eawebmaster20 <eaweb20@gmail.com>
2024-08-28 16:20:17 +0000

    add routes for navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { InvoiceDetailsPageComponent } from './pages/invoice-details-page/invoice-details-page.component';
+import { FormComponent } from './shared/components/form/form.component';
 
 export const routes: Routes = [
-    {path: '', component:HomePageComponent},
-    {path:'details', component:InvoiceDetailsPageComponent}
+    {path: '', component:FormComponent},
+    {path:'details/:id', component:InvoiceDetailsPageComponent}
 ];
