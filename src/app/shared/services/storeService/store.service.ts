cba21600ff83cbe477b90e456a62cc451862b9e5
eawebmaster20 <eaweb20@gmail.com>
2024-08-29 17:10:41 +0000

    add reactive form validators in store service

--- src/app/shared/services/storeService/store.service.ts
+++ src/app/shared/services/storeService/store.service.ts
@@ -4,6 +4,7 @@ import { Invoice } from '../../models/store.types';
 import { InvoiceState } from '../../state/invoice.entity';
 import { Store } from '@ngrx/store';
 import * as InvoiceActions from '../../state/invoice.action';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 @Injectable({
   providedIn: 'root'
 })
@@ -11,19 +12,50 @@ export class StoreService {
 
   constructor(
     private apiService:ApiService,
-    private store: Store<InvoiceState>
+    private store: Store<InvoiceState>,
+    private fb: FormBuilder
   ) { }
-
+  createdAt = new FormControl('', [Validators.required]);
+  paymentDue = new FormControl('', [Validators.required]);
+  description = new FormControl('', [Validators.required]);
+  paymentTerms = new FormControl(1, [Validators.required, Validators.min(1)]);
+  clientEmail = new FormControl('', [Validators.required, Validators.email]);
+  clientName = new FormControl('', [Validators.required]);
+  senderStreet=new FormControl( ['', Validators.required]);
+  senderCity= new FormControl(['', Validators.required]);
+  senderPostCode=new FormControl (['', Validators.required]);
+  senderCountry= new FormControl(['', Validators.required]);
+  clientStreet=new FormControl( ['', Validators.required]);
+  clientCity= new FormControl(['', Validators.required]);
+  clientPostCode=new FormControl (['', Validators.required]);
+  clientCountry= new FormControl(['', Validators.required]);
+  items = new FormControl(0, [Validators.required]);
+  total = new FormControl(0, [Validators.required]);
+  newInvoice: Invoice= {
+    id: "",
+    createdAt: "",
+    paymentDue: "",
+    description: "",
+    paymentTerms: 1,
+    clientName: "",
+    clientEmail: "",
+    status: "",
+    senderAddress: {
+      street: "",
+      city: "",
+      postCode: "",
+      country: ""
+    },
+    clientAddress: {
+      street: "",
+      city: "",
+      postCode: "",
+      country: ""
+    },
+    items: [],
+    total: 0
+  }
   getInvoices(){
-    // this.apiService.fetchInvoices().subscribe(
-    //   (invoices) => {
-    //     this.store.dispatch(InvoiceActions.fetchInvoicesSuccess({invoices}));
-    //   },
-    //   (error) => {
-    //     this.store.dispatch(InvoiceActions.fetchInvoicesFailure(error));
-    //     console.error(error)
-    //   }
-    // )
   }
   
   addInvoice(invoice: Invoice){
