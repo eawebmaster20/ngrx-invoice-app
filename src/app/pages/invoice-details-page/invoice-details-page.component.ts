c9e590bba0c316f1e09c0909695ebd0c0a91307b
eawebmaster20 <eaweb20@gmail.com>
2024-08-31 09:06:12 +0000

    modify invoice page

--- src/app/pages/invoice-details-page/invoice-details-page.component.ts
+++ src/app/pages/invoice-details-page/invoice-details-page.component.ts
@@ -4,17 +4,27 @@ import { selectInvoiceById } from '../../shared/state/invoice.selectors';
 import { Invoice } from '../../shared/models/store.types';
 import { Observable } from 'rxjs';
 import { MatButtonModule } from '@angular/material/button';
+import { AsyncPipe } from '@angular/common';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { InvoiceDetailContentComponent } from './invoice-detail-content/invoice-detail-content.component';
+import { InvoiceDetailHeaderComponent } from './invoice-detail-header/invoice-detail-header.component';
 
 @Component({
   selector: 'app-invoice-details-page',
   standalone: true,
-  imports: [MatButtonModule,],
+  imports: [
+    MatButtonModule,
+    AsyncPipe, 
+    InvoiceDetailHeaderComponent, 
+    InvoiceDetailContentComponent, 
+    RouterLink
+  ],
   templateUrl: './invoice-details-page.component.html',
   styleUrl: './invoice-details-page.component.scss'
 })
 export class InvoiceDetailsPageComponent {
-  selectedInvoice:Observable<any> = this.store.select(selectInvoiceById('RG0314'))
-  constructor(private store:Store){
+  selectedInvoice:Observable<any> = this.store.select(selectInvoiceById(this.route.snapshot.paramMap.get('id')!))
+  constructor(private route: ActivatedRoute, private store:Store){
     this.selectedInvoice.subscribe(invoice =>console.log(invoice))
   }
 }
